b31a73f24ba1b4b98014106c2bfbb8f76f51ec74
BlackSun225 <100361442+BlackSun225@users.noreply.github.com>
2023-02-20 10:29:12 +0000

    files added

---
+++ loopstudios.js
@@ -0,0 +1,6 @@
+document.getElementById('open').addEventListener('click', ()=>{
+  document.getElementsByClassName('menu')[0].style.display = "block";
+})
+document.getElementById('close').addEventListener('click', ()=>{
+  document.getElementsByClassName('menu')[0].style.display = "none";
+})
